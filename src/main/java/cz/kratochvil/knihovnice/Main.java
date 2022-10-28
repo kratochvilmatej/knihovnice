d1b3d72a95d44f35b0e78ad8bc88c0ecfebdec55
prach <matej.kratochvil@student.gyarab.cz>
2022-10-28 15:29:33 +0200

    Error opraven

--- src/main/java/cz/kratochvil/knihovnice/Main.java
+++ src/main/java/cz/kratochvil/knihovnice/Main.java
@@ -2,18 +2,17 @@ package cz.kratochvil.knihovnice;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/resources/Login.fxml"));
-        stage.setScene(new Scene(root, 400, 400));
-        stage.show();
+    public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/cz/kratochvil/knihovnice/Login.fxml")));
+            stage.setScene(new Scene(root, 400, 400));
+            stage.show();
     }
     public static void main(String[] args) {
         launch(args);
