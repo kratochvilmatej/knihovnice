1d77015227d9d811497e2355d79fb8b5fc9844e9
prach <matej.kratochvil@student.gyarab.cz>
2022-11-02 23:18:51 +0100

    nevim proste uz to funguje

--- src/main/java/cz/kratochvil/knihovnice/Main.java
+++ src/main/java/cz/kratochvil/knihovnice/Main.java
@@ -6,16 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class Main extends Application {
     public void start(Stage stage) throws Exception {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login.fxml")));
-            stage.setScene(new Scene(root, 400, 400));
-            stage.show();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login.fxml")));
+        stage.setScene(new Scene(root, 400, 400));
+        stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
 }
+
--- src/main/java/cz/kratochvil/knihovnice/MainController.java
+++ src/main/java/cz/kratochvil/knihovnice/MainController.java
@@ -11,7 +11,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.net.ssl.HandshakeCompletedEvent;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class MainController {
@@ -19,21 +26,115 @@ public class MainController {
     private Label lblStat;
 
     @FXML
+    private Label lblStatReg;
+
+    @FXML
     private TextField txUser;
 
     @FXML
     private TextField txPass;
 
-   /* public void Login(ActionEvent event) {
-        if (txUser.getText().equals("user") && txPass.getText().equals("pass")) {
+    public String usernames = "";
+
+    public String passwords = "";
+
+    public HashMap<String, String> loginy = new HashMap<>();
+
+    public void Login(ActionEvent event) {
+
+
+        try {
+            File file = new File("src/main/resources/cz/kratochvil/knihovnice/usernames.txt");
+            FileReader reader = new FileReader(file);
+            BufferedReader bfreader = new BufferedReader(reader);
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = bfreader.readLine()) != null) {
+                sb.append(line);
+                //sb.append("\n");
+            }
+            reader.close();
+            usernames = sb.toString();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        String[] username = usernames.split("\\s+");
+
+        try {
+            File file = new File("src/main/resources/cz/kratochvil/knihovnice/passwords.txt");
+            FileReader reader = new FileReader(file);
+            BufferedReader bfreader = new BufferedReader(reader);
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = bfreader.readLine()) != null) {
+                sb.append(line);
+                //sb.append("\n");
+            }
+            reader.close();
+            passwords = sb.toString();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        String[] password = passwords.split("\\s+");
+
+        for (int x = 0; x < username.length; x++) {
+            loginy.put(username[x], password[x]);
+        }
+        for (String i : loginy.keySet()) {
+            System.out.println(i + " " + loginy.get(i));
+        }
+
+        String heslo = loginy.get(txUser.getText());
+
+        if (txPass.getText().equals(heslo)) {
             lblStat.setTextFill(Color.rgb(0, 255, 0, 1));
             lblStat.setText("Příhlášení Úspešné");
+            String loggedUser = txUser.getText();
+            //prepni na knihovnu
         } else {
             lblStat.setTextFill(Color.rgb(255, 0, 0, 1));
             lblStat.setText("Nesprávné Uživatelské Jméno nebo Heslo");
         }
+    }
 
-    } */
+    public void Register(ActionEvent event) {
+        if (!txUser.getText().contains("\\s+") || !txPass.getText().contains("\\s+")) {
+            if (!txUser.getText().isBlank() && !txPass.getText().isBlank()) {
+                try {
+                    prepniLogin(event);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String regUsername = txUser.getText();
+                String regPassword = txPass.getText();
+
+                try {
+                    /*BufferedWriter writer = new BufferedWriter(new FileWriter("usernames.txt"));
+                    writer.write(usernames + regUsername);
+                    writer.close();*/
+                    Files.writeString(Paths.get("src/main/resources/cz/kratochvil/knihovnice/usernames.txt"), regUsername + " ", StandardOpenOption.APPEND);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    /*BufferedWriter writer = new BufferedWriter(new FileWriter("passwords.txt"));
+                    writer.write(passwords + regPassword);
+                    writer.close();*/
+                    Files.writeString(Paths.get("src/main/resources/cz/kratochvil/knihovnice/passwords.txt"), regPassword + " ", StandardOpenOption.APPEND);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                lblStat.setTextFill(Color.rgb(255, 0, 0, 1));
+                lblStat.setText("Uživatelské Jméno ani Heslo nesmí být prázdné");
+            }
+        } else {
+            lblStat.setTextFill(Color.rgb(255, 0, 0, 1));
+            lblStat.setText("Uživatelské Jméno ani Heslo nesmí obsahovat mezery!");
+        }
+    }
 
 
     public void Reset(ActionEvent event) {
@@ -42,7 +143,7 @@ public class MainController {
         lblStat.setText("");
     }
 
-    File data = new File("data.txt");
+    /*File data = new File("data.txt");
     int ln;
     String Username;
     String Password;
@@ -154,7 +255,7 @@ public class MainController {
         readFile();
         countLines();
         addData(txUser.getText(), txPass.getText());
-    }
+    }*/
 
     private Stage stage;
     private Scene scene;
