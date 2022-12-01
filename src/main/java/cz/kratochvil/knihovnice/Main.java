90b5214285f525cd864062e8ef339554241e8d5d
prach <matej.kratochvil@student.gyarab.cz>
2022-12-01 20:41:32 +0100

    Ikona

--- src/main/java/cz/kratochvil/knihovnice/Main.java
+++ src/main/java/cz/kratochvil/knihovnice/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.*;
@@ -13,6 +14,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
         stage.setScene(new Scene(root, 1080, 720));
+        stage.getIcons().add(new Image("file:src/main/resources/cz/kratochvil/knihovnice/ikona.png"));
         stage.show();
     }
 
